5f43e1f3e757bdd6726266696db4f538f0e726b5
antoniopurificato <purificato.2019135@studenti.uniroma1.it>
2022-10-31 17:02:51 +0100

    First work with the code.

--- first.py
+++ first.py
@@ -1,15 +1,14 @@
 import os
 import torch
-####
+
 import random
 import time
 from math import log, log2
 import matplotlib.pyplot as plt
 
-
 import numpy as np
 import pandas as pd
-#import networkx as nx
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -20,3 +19,101 @@ from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.utils import degree
 from torch_scatter import scatter_mean, scatter_max, scatter_sum, scatter_add
 from torch_geometric.utils import to_dense_adj, to_undirected, remove_self_loops
+
+from dataset import *
+from models import *
+from evaluation import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+u_t = torch.LongTensor(train_df.user_id_idx)
+i_t = torch.LongTensor(train_df.item_id_idx) + n_users
+
+train_edge_index = torch.stack((
+  torch.cat([u_t, i_t]),
+  torch.cat([i_t, u_t])
+)).to(device)
+
+latent_dim = 64
+n_layers = 20 
+EPOCHS = 90
+BATCH_SIZE = 1024
+DECAY = 0.0001
+LR = 0.005 
+K = 100
+
+def train_and_eval(model, optimizer, train_df):
+  loss_list_epoch = []
+  bpr_loss_list_epoch = []
+  reg_loss_list_epoch = []
+  ndgc_list = []
+
+  recall_list = []
+  precision_list = []
+
+  for epoch in tqdm(range(EPOCHS)): 
+      n_batch = int(len(train)/BATCH_SIZE)
+    
+      final_loss_list = []
+      bpr_loss_list = []
+      reg_loss_list = []
+    
+      model.train()
+      for batch_idx in range(n_batch):
+          print
+
+          optimizer.zero_grad()
+
+          users, pos_items, neg_items = data_loader(train_df, BATCH_SIZE, n_users, n_items)
+          users_emb, pos_emb, neg_emb, userEmb0,  posEmb0, negEmb0 = model.encode_minibatch(users, pos_items, neg_items, train_edge_index)
+
+          bpr_loss, reg_loss = compute_bpr_loss(
+            users, users_emb, pos_emb, neg_emb, userEmb0,  posEmb0, negEmb0
+          )
+          reg_loss = DECAY * reg_loss
+          final_loss = bpr_loss + reg_loss
+
+          final_loss.backward()
+          optimizer.step()
+
+          final_loss_list.append(final_loss.item())
+          bpr_loss_list.append(bpr_loss.item())
+          reg_loss_list.append(reg_loss.item())
+
+      model.eval()
+      with torch.no_grad():
+          _, out = model(train_edge_index)
+          final_user_Embed, final_item_Embed = torch.split(out, (n_users, n_items))
+          test_topK_recall,  test_topK_precision, ndgc = get_metrics(
+            final_user_Embed, final_item_Embed, n_users, n_items, train_df, test_df, K
+          )
+
+      loss_list_epoch.append(round(np.mean(final_loss_list),4))
+      bpr_loss_list_epoch.append(round(np.mean(bpr_loss_list),4))
+      reg_loss_list_epoch.append(round(np.mean(reg_loss_list),4))
+      ndgc_list.append(round(np.mean(ndgc),4))
+
+      recall_list.append(round(test_topK_recall,4))
+      precision_list.append(round(test_topK_precision,4))
+
+  return (
+    loss_list_epoch, 
+    bpr_loss_list_epoch, 
+    reg_loss_list_epoch, 
+    recall_list, 
+    precision_list,
+    ndgc_list
+  )
+
+
+lightgcn = RecSysGNN(
+  latent_dim=latent_dim, 
+  num_layers=n_layers,
+  num_users=n_users,
+  num_items=n_items,
+  model='LightGCN'
+)
+lightgcn.to(device)
+
+optimizer = torch.optim.Adam(lightgcn.parameters(), lr=LR)
+light_loss, light_bpr, light_reg, light_recall, light_precision, light_ndcg = train_and_eval(lightgcn, optimizer, train_df)
